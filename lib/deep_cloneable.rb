dced490b4aadd2b06bd40cfcdd04da7388642b72
Reinier de Lange <r.j.delange@nedforce.nl>
2013-06-05 16:47:37 +0200

    Possible fix for initialize_dup error

--- lib/deep_cloneable.rb
+++ lib/deep_cloneable.rb
@@ -3,12 +3,12 @@ class ActiveRecord::Base
     # ActiveRecord::Base has its own dup method for Ruby 1.8.7. We have to
     # redefine it and put it in a module so that we can override it in a
     # module and call the original with super().
-    if !Object.respond_to? :initialize_dup, true
+    unless Object.respond_to?(:initialize_dup, true)
       ActiveRecord::Base.class_eval do
         module Dup
           def dup
             copy = super
-            copy.initialize_dup(self)
+            copy.send(:initialize_dup, self)
             copy
           end
         end
